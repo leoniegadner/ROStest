0ff178787712c81880803e237485f4c040a3df93
Leonie Gadner <leonie.gadner@gmx.at>
2023-01-22 17:59:13 +0900

    got rid of global parameter, service and publisher in one node

--- src/homework/homework.cpp
+++ src/homework/homework.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <sstream>
 
-int add(test1::add::Request  &req,
+bool add(test1::add::Request  &req,
          test1::add::Response &res, int Y)
 {
     ros::NodeHandle n;
@@ -14,7 +14,7 @@ int add(test1::add::Request  &req,
     n.setParam("msg", res.sum);
     ROS_INFO("request: x=%ld", (long int)req.X);
     ROS_INFO("sending back response: [%ld]", (long int)res.sum);
-    return res.sum;
+    return true;
 }
 
 //int main(int argc, char **argv)
@@ -32,6 +32,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "publisher_service");
     ros::NodeHandle n;
+    ros::
     ros::Publisher publisher = n.advertise<std_msgs::Int32>("publisher", 1000);
     ros::ServiceServer service = n.advertiseService("add_service", add);
     //n.setParam("msg", 0);
@@ -45,9 +46,11 @@ int main(int argc, char **argv)
         //msg.data = x;
         //ROS_INFO("%d", x);
         publisher.publish(msg);
+        if (ros::service::call("service", add, msg.data)){
+            ROS_INFO("test")
+        }
         //std::cout << msg.data;
         //ROS_INFO("I said: [%s]", msg->data.c_str());
-
 
         ros::spinOnce();
         loop_rate.sleep();
